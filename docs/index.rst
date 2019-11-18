1fe629b6c928ae5515cc25264847cf0d29e0e5ce
Asli Ozcan <asli.ozcan@basestech.com>
2019-11-19 00:17:00 +0300

    sphinx docs are added

---
+++ docs/index.rst
@@ -0,0 +1,21 @@
+
+
+Welcome to neuralNetwork's documentation!
+=========================================
+
+.. automodule:: neuralNetwork.test
+    :members:
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- test.py
+++ test.py
@@ -10,6 +10,14 @@ from sklearn.preprocessing import Imputer
 from sklearn.preprocessing import OneHotEncoder
 
 
+
+
+"""
+test.py
+====================================
+The core module of my example project
+"""
+
 if __name__ == "__main__":
     
     # trainX = np.random.randn(1000, 13) # number of samples, number of features
